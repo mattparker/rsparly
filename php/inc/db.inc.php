9956fba17942b4ebd67cde070dca91d141124229
Matt Parker <matt@lamplightdb.co.uk>
2011-11-05 20:26:36 +0000

    Checked with some test data, seems to be working.  Extra LIKE search

---
+++ php/inc/db.inc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Some globals for the database connection
+ *
+ */
+
+define ("PARLY_SERVER", "localhost:3306");
+define ("PARLY_USER", "root");
+define ("PARLY_PASSWORD", "");
+define ("PARLY_DB", "parlyconnect");
